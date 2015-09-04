3415ab485ff48df0550519b630f6bf6b4bdbae25
Roger Pau Monne <roger.pau@citrix.com>
2015-09-04 13:06:49 +0200

    xen/x86: allow HVM guests to use hypercalls to bring up vCPUs
    Allow the usage of the VCPUOP_initialise, VCPUOP_up, VCPUOP_down and
    VCPUOP_is_up hypercalls from HVM guests.
    
    This patch introduces a new structure (vcpu_hvm_context) that should be used
    in conjuction with the VCPUOP_initialise hypercall in order to initialize
    vCPUs for HVM guests.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Ian Campbell <ian.campbell@citrix.com>
    Cc: Stefano Stabellini <stefano.stabellini@citrix.com>
    ---
    Changes since v7:
     - Improved error messages.
     - Set EFER.LMA if EFER.LME is set by the user.
     - Fix calculation of CS limit.
    
    Changes since v6:
     - Add comments to clarify some initializations.
     - Introduce a generic default_initialize_vcpu that's used to initialize a
       ARM vCPU or a x86 PV vCPU.
     - Move the undef of the SEG macro.
     - Fix the size of the eflags register, it should be 32bits.
     - Add a comment regarding the value of the 12-15 bits of the _ar fields.
     - Remove the 16bit strucutre, the 32bit one can be used to start the cpu in
       real mode.
     - Add some sanity checks to the values passed in.
     - Add paddings to vcpu_hvm_context so the layout on 32/64bits is the same.
     - Add support for the compat version of VCPUOP_initialise.
    
    Changes since v5:
     - Fix a coding style issue.
     - Merge the code from wip-dmlite-v5-refactor by Andrew in order to reduce
       bloat.
     - Print the offending %cr3 in case of error when using shadow.
     - Reduce the scope of local variables in arch_initialize_vcpu.
     - s/current->domain/v->domain/g in arch_initialize_vcpu.
     - Expand the comment in public/vcpu.h to document the usage of
       vcpu_hvm_context for HVM guests.
     - Add myself as the copyright holder for the public hvm_vcpu.h header.
    
    Changes since v4:
     - Don't assume mode is 64B, add an explicit check.
     - Don't set TF_kernel_mode, it is only needed for PV guests.
     - Don't set CR0_ET unconditionally.

--- xen/common/compat/domain.c
+++ xen/common/compat/domain.c
@@ -10,6 +10,9 @@
 #include <xen/guest_access.h>
 #include <xen/hypercall.h>
 #include <compat/vcpu.h>
+#ifdef CONFIG_X86
+#include <compat/hvm/hvm_vcpu.h>
+#endif
 
 #define xen_vcpu_set_periodic_timer vcpu_set_periodic_timer
 CHECK_vcpu_set_periodic_timer;
@@ -22,6 +25,41 @@ CHECK_SIZE_(struct, vcpu_info);
 #define xen_vcpu_register_vcpu_info vcpu_register_vcpu_info
 CHECK_vcpu_register_vcpu_info;
 #undef xen_vcpu_register_vcpu_info
+
+#ifdef CONFIG_X86
+#define xen_vcpu_hvm_context vcpu_hvm_context
+#define xen_vcpu_hvm_x86_32 vcpu_hvm_x86_32
+#define xen_vcpu_hvm_x86_64 vcpu_hvm_x86_64
+CHECK_vcpu_hvm_context;
+#undef xen_vcpu_hvm_x86_64
+#undef xen_vcpu_hvm_x86_32
+#undef xen_vcpu_hvm_context
+#endif
+
+static int default_initialize_vcpu(struct vcpu *v,
+                                   XEN_GUEST_HANDLE_PARAM(void) arg)
+{
+    struct compat_vcpu_guest_context *cmp_ctxt;
+    struct domain *d = v->domain;
+    int rc;
+
+    if ( (cmp_ctxt = xmalloc(struct compat_vcpu_guest_context)) == NULL )
+            return -ENOMEM;
+
+    if ( copy_from_guest(cmp_ctxt, arg, 1) )
+    {
+        xfree(cmp_ctxt);
+        return -EFAULT;
+    }
+
+    domain_lock(d);
+    rc = v->is_initialised ? -EEXIST : arch_set_info_guest(v, cmp_ctxt);
+    domain_unlock(d);
+
+    xfree(cmp_ctxt);
+
+    return rc;
+}
 
 int compat_vcpu_op(int cmd, unsigned int vcpuid, XEN_GUEST_HANDLE_PARAM(void) arg)
 {
@@ -36,33 +74,36 @@ int compat_vcpu_op(int cmd, unsigned int vcpuid, XEN_GUEST_HANDLE_PARAM(void) ar
     {
     case VCPUOP_initialise:
     {
-        struct compat_vcpu_guest_context *cmp_ctxt;
-
         if ( v->vcpu_info == &dummy_vcpu_info )
             return -EINVAL;
 
-        if ( (cmp_ctxt = xmalloc(struct compat_vcpu_guest_context)) == NULL )
+#if defined(CONFIG_X86)
+        if ( is_hvm_vcpu(v) )
         {
-            rc = -ENOMEM;
-            break;
-        }
+            struct vcpu_hvm_context hvm_ctx;
 
-        if ( copy_from_guest(cmp_ctxt, arg, 1) )
+            if ( copy_from_guest(&hvm_ctx, arg, 1) )
+                return -EFAULT;
+
+            domain_lock(d);
+            rc = v->is_initialised ? -EEXIST :
+                                     arch_set_info_hvm_guest(v, &hvm_ctx);
+            domain_unlock(d);
+        }
+        else
         {
-            xfree(cmp_ctxt);
-            rc = -EFAULT;
-            break;
+            rc = default_initialize_vcpu(v, arg);
         }
-
-        domain_lock(d);
-        rc = v->is_initialised ? -EEXIST : arch_set_info_guest(v, cmp_ctxt);
-        domain_unlock(d);
+#elif defined(CONFIG_ARM)
+        rc = default_initialize_vcpu(v, arg);
+#else
+        #error Unsupported architecture
+#endif
 
         if ( rc == -ERESTART )
             rc = hypercall_create_continuation(__HYPERVISOR_vcpu_op, "iuh",
                                                cmd, vcpuid, arg);
 
-        xfree(cmp_ctxt);
         break;
     }
 
