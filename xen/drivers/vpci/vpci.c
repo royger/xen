00586f37219a49f658abb8c9a1ccb5cab54c7f15
Roger Pau Monne <roger.pau@citrix.com>
2017-04-18 12:10:16 +0100

    vpci: add a priority field to the vPCI register initializer
    And mark the capability and header vPCI register initializer as high priority,
    so that they are initialized first.
    
    This is needed for MSI-X, since MSI-X needs to know the position of the BARs in
    order to perform it's initialization, and in order to mask or enable the
    MSI/MSI-X functionality on demand.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -20,7 +20,7 @@
 #include <xen/sched.h>
 #include <xen/vpci.h>
 
-extern const vpci_register_init_t __start_vpci_array[], __end_vpci_array[];
+extern const struct vpci_register_init __start_vpci_array[], __end_vpci_array[];
 #define NUM_VPCI_INIT (__end_vpci_array - __start_vpci_array)
 #define vpci_init __start_vpci_array
 
@@ -42,6 +42,7 @@ struct vpci_register {
 int xen_vpci_add_handlers(struct pci_dev *pdev)
 {
     int i, rc = 0;
+    bool priority = true;
 
     if ( !has_vpci(pdev->domain) )
         return 0;
@@ -52,9 +53,13 @@ int xen_vpci_add_handlers(struct pci_dev *pdev)
 
     pdev->vpci->handlers = RB_ROOT;
 
+ again:
     for ( i = 0; i < NUM_VPCI_INIT; i++ )
     {
-        rc = vpci_init[i](pdev);
+        if ( priority != vpci_init[i].priority )
+            continue;
+
+        rc = vpci_init[i].init(pdev);
         if ( rc )
             break;
     }
@@ -73,6 +78,11 @@ int xen_vpci_add_handlers(struct pci_dev *pdev)
             xfree(r);
         }
         xfree(pdev->vpci);
+    }
+    else if ( priority )
+    {
+        priority = false;
+        goto again;
     }
 
     return rc;
