eb803cada0b15e930c128c932dd918fec7dcf342
Roger Pau Monne <roger.pau@citrix.com>
2017-04-20 10:27:57 +0100

    xen/vpci: add handlers to map the BARs
    Introduce a set of handlers that trap accesses to the PCI BARs and the command
    register, in order to emulate BAR sizing and BAR relocation.
    
    The command handler is used to detect changes to bit 2 (response to memory
    space accesses), and maps/unmaps the BARs of the device into the guest p2m.
    
    The BAR register handlers are used to detect attempts by the guest to size or
    relocate the BARs.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: George Dunlap <George.Dunlap@eu.citrix.com>
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Tim Deegan <tim@xen.org>
    Cc: Wei Liu <wei.liu2@citrix.com>

--- xen/include/xen/vpci.h
+++ xen/include/xen/vpci.h
@@ -50,6 +50,33 @@ int xen_vpci_write(unsigned int seg, unsigned int bus, unsigned int devfn,
 struct vpci {
     /* Root pointer for the tree of vPCI handlers. */
     struct rb_root handlers;
+
+#ifdef __XEN__
+    /* Hide the rest of the vpci struct from the user-space test harness. */
+    struct vpci_header {
+        /* Cached value of the command register. */
+        uint16_t command;
+        /* Information about the PCI BARs of this device. */
+        struct vpci_bar {
+            enum {
+                VPCI_BAR_EMPTY,
+                VPCI_BAR_IO,
+                VPCI_BAR_MEM,
+                VPCI_BAR_MEM64_LO,
+                VPCI_BAR_MEM64_HI,
+            } type;
+            /* Hardware address. */
+            paddr_t paddr;
+            /* Guest address where the BAR should be mapped. */
+            paddr_t gaddr;
+            /* Current guest address where the BAR is mapped. */
+            paddr_t mapped_addr;
+            size_t size;
+            unsigned int attributes:4;
+            bool sizing;
+        } bars[6];
+    } header;
+#endif
 };
 
 #endif
