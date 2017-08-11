7f05a9eba76ba7f50c2044a9eca725a80a7fdcfa
Roger Pau Monne <roger.pau@citrix.com>
2017-08-11 16:32:32 +0100

    pci: split code to size BARs from pci_add_device
    So that it can be called from outside in order to get the size of regular PCI
    BARs. This will be required in order to map the BARs from PCI devices into PVH
    Dom0 p2m.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    Cc: Jan Beulich <jbeulich@suse.com>
    ---
    Changes since v4:
     - Restore printing whether the BAR is from a vf.
     - Make the psize pointer parameter not optional.
     - s/u64/uint64_t.
     - Remove some unneeded parentheses.
     - Assert the return value is never 0.
    
    Changes since v3:
     - Rename function to size BARs to pci_size_mem_bar.
     - Change the parameters passed to the function. Pass the position and
       whether the BAR is the last one, instead of the (base, max_bars,
       *index) tuple.
     - Make the function return the number of BARs consumed (1 for 32b, 2
       for 64b BARs).
     - Change the dprintk back to printk.
     - Do not log another error message in pci_add_device in case
       pci_size_mem_bar fails.

--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -164,6 +164,9 @@ const char *parse_pci(const char *, unsigned int *seg, unsigned int *bus,
                       unsigned int *dev, unsigned int *func);
 const char *parse_pci_seg(const char *, unsigned int *seg, unsigned int *bus,
                           unsigned int *dev, unsigned int *func, bool *def_seg);
+int pci_size_mem_bar(unsigned int seg, unsigned int bus, unsigned int slot,
+                     unsigned int func, unsigned int pos, bool last,
+                     uint64_t *addr, uint64_t *size, bool vf);
 
 
 bool_t pcie_aer_get_firmware_first(const struct pci_dev *);
