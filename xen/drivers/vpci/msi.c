52b6dd40532debf3a45535df937ee2a2d1dbe69e
Roger Pau Monne <roger.pau@citrix.com>
2017-09-18 15:04:24 +0100

    vpci/msi: add MSI handlers
    Add handlers for the MSI control, address, data and mask fields in
    order to detect accesses to them and setup the interrupts as requested
    by the guest.
    
    Note that the pending register is not trapped, and the guest can
    freely read/write to it.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Paul Durrant <paul.durrant@citrix.com>
    ---
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Paul Durrant <paul.durrant@citrix.com>
    ---
    Changes since v5:
     - Update to new lock usage.
     - Change handlers to match the new type.
     - s/msi_flags/msi_gflags/, remove the local variables and use the new
       DOMCTL_VMSI_* defines.
     - Change the MSI arch function to take a vpci_msi instead of a
       vpci_arch_msi as parameter.
     - Fix the calculation of the guest vector for MSI injection to take
       into account the number of bits that can be modified.
     - Use INVALID_PIRQ everywhere.
     - Simplify exit path of vpci_msi_disable.
     - Remove the conditional when setting address64 and masking fields.
     - Add a process_pending_softirqs to the MSI dump loop.
     - Place the prototypes for the MSI arch-specific functions in
       xen/vpci.h.
     - Add parentheses around the INVALID_PIRQ definition.
    
    Changes since v4:
     - Fix commit message.
     - Change the ASSERTs in vpci_msi_arch_mask into ifs.
     - Introduce INVALID_PIRQ.
     - Destroy the partially created bindings in case of failure in
       vpci_msi_arch_enable.
     - Just take the pcidevs lock once in vpci_msi_arch_disable.
     - Print an error message in case of failure of pt_irq_destroy_bind.
     - Make vpci_msi_arch_init return void.
     - Constify the arch parameter of vpci_msi_arch_print.
     - Use fixed instead of cpu for msi redirection.
     - Separate the header includes in vpci/msi.c between xen and asm.
     - Store the number of configured vectors even if MSI is not enabled
       and always return it in vpci_msi_control_read.
     - Fix/add comments in vpci_msi_control_write to clarify intended
       behavior.
     - Simplify usage of masks in vpci_msi_address_{upper_}write.
     - Add comment to vpci_msi_mask_{read/write}.
     - Don't use MASK_EXTR in vpci_msi_mask_write.
     - s/msi_offset/pos/ in vpci_init_msi.
     - Move control variable setup closer to it's usage.
     - Use d%d in vpci_dump_msi.
     - Fix printing of bitfield mask in vpci_dump_msi.
     - Fix definition of MSI_ADDR_REDIRECTION_MASK.
     - Shuffle the layout of vpci_msi to minimize gaps.
     - Remove the error label in vpci_init_msi.
    
    Changes since v3:
     - Propagate changes from previous versions: drop xen_ prefix, drop
       return value from handlers, use the new vpci_val fields.
     - Use MASK_EXTR.
     - Remove the usage of GENMASK.
     - Add GFLAGS_SHIFT_DEST_ID and use it in msi_flags.
     - Add "arch" to the MSI arch specific functions.
     - Move the dumping of vPCI MSI information to dump_msi (key 'M').
     - Remove the guest_vectors field.
     - Allow the guest to change the number of active vectors without
       having to disable and enable MSI.
     - Check the number of active vectors when parsing the disable
       mask.
     - Remove the debug messages from vpci_init_msi.
     - Move the arch-specific part of the dump handler to x86/hvm/vmsi.c.
     - Use trylock in the dump handler to get the vpci lock.
    
    Changes since v2:
     - Add an arch-specific abstraction layer. Note that this is only implemented
       for x86 currently.
     - Add a wrapper to detect MSI enabling for vPCI.
    
    NB: I've only been able to test this with devices using a single MSI interrupt
    and no mask register. I will try to find hardware that supports the mask
    register and more than one vector, but I cannot make any promises.
    
    If there are doubts about the untested parts we could always force Xen to
    report no per-vector masking support and only 1 available vector, but I would
    rather avoid doing it.

---
+++ xen/drivers/vpci/msi.c
@@ -0,0 +1,366 @@
+/*
+ * Handlers for accesses to the MSI capability structure.
+ *
+ * Copyright (C) 2017 Citrix Systems R&D
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms and conditions of the GNU General Public
+ * License, version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <xen/sched.h>
+#include <xen/softirq.h>
+#include <xen/vpci.h>
+
+#include <asm/msi.h>
+
+static uint32_t vpci_msi_control_read(const struct pci_dev *pdev,
+                                      unsigned int reg, void *data)
+{
+    const struct vpci_msi *msi = data;
+    uint16_t val;
+
+    /* Set the number of supported/configured messages. */
+    val = MASK_INSR(fls(msi->max_vectors) - 1, PCI_MSI_FLAGS_QMASK);
+    val |= MASK_INSR(fls(msi->vectors) - 1, PCI_MSI_FLAGS_QSIZE);
+
+    val |= msi->enabled ? PCI_MSI_FLAGS_ENABLE : 0;
+    val |= msi->masking ? PCI_MSI_FLAGS_MASKBIT : 0;
+    val |= msi->address64 ? PCI_MSI_FLAGS_64BIT : 0;
+
+    return val;
+}
+
+static void vpci_msi_enable(const struct pci_dev *pdev, struct vpci_msi *msi,
+                            unsigned int vectors)
+{
+    int ret;
+
+    ASSERT(!msi->enabled);
+    ret = vpci_msi_arch_enable(msi, pdev, vectors);
+    if ( ret )
+        return;
+
+    /* Apply the mask bits. */
+    if ( msi->masking )
+    {
+        unsigned int i;
+        uint32_t mask = msi->mask;
+
+        for ( i = ffs(mask) - 1; mask && i < vectors; i = ffs(mask) - 1 )
+        {
+            vpci_msi_arch_mask(msi, pdev, i, true);
+            __clear_bit(i, &mask);
+        }
+    }
+
+    __msi_set_enable(pdev->seg, pdev->bus, PCI_SLOT(pdev->devfn),
+                     PCI_FUNC(pdev->devfn), msi->pos, 1);
+
+    msi->enabled = true;
+}
+
+static int vpci_msi_disable(const struct pci_dev *pdev, struct vpci_msi *msi)
+{
+    int ret;
+
+    ASSERT(msi->enabled);
+    __msi_set_enable(pdev->seg, pdev->bus, PCI_SLOT(pdev->devfn),
+                     PCI_FUNC(pdev->devfn), msi->pos, 0);
+
+    ret = vpci_msi_arch_disable(msi, pdev);
+    if ( !ret )
+        msi->enabled = false;
+
+    return ret;
+}
+
+static void vpci_msi_control_write(const struct pci_dev *pdev,
+                                   unsigned int reg, uint32_t val, void *data)
+{
+    struct vpci_msi *msi = data;
+    unsigned int vectors = 1 << MASK_EXTR(val, PCI_MSI_FLAGS_QSIZE);
+    bool new_enabled = val & PCI_MSI_FLAGS_ENABLE;
+
+    if ( vectors > msi->max_vectors )
+        vectors = msi->max_vectors;
+
+    /*
+     * No change if the enable field and the number of vectors is
+     * the same or the device is not enabled, in which case the
+     * vectors field can be updated directly.
+     */
+    if ( new_enabled == msi->enabled &&
+         (vectors == msi->vectors || !msi->enabled) )
+    {
+        msi->vectors = vectors;
+        return;
+    }
+
+    if ( new_enabled )
+    {
+        /*
+         * If the device is already enabled it means the number of
+         * enabled messages has changed. Disable and re-enable the
+         * device in order to apply the change.
+         */
+        if ( msi->enabled && vpci_msi_disable(pdev, msi) )
+            /*
+             * Somehow Xen has not been able to disable the
+             * configured MSI messages, leave the device state as-is,
+             * so that the guest can try to disable MSI again.
+             */
+            return;
+
+        vpci_msi_enable(pdev, msi, vectors);
+    }
+    else
+        vpci_msi_disable(pdev, msi);
+
+    msi->vectors = vectors;
+}
+
+/* Handlers for the address field (32bit or low part of a 64bit address). */
+static uint32_t vpci_msi_address_read(const struct pci_dev *pdev,
+                                      unsigned int reg, void *data)
+{
+    const struct vpci_msi *msi = data;
+
+    return msi->address;
+}
+
+static void vpci_msi_address_write(const struct pci_dev *pdev,
+                                   unsigned int reg, uint32_t val, void *data)
+{
+    struct vpci_msi *msi = data;
+
+    /* Clear low part. */
+    msi->address &= ~0xffffffffull;
+    msi->address |= val;
+}
+
+/* Handlers for the high part of a 64bit address field. */
+static uint32_t vpci_msi_address_upper_read(const struct pci_dev *pdev,
+                                            unsigned int reg, void *data)
+{
+    const struct vpci_msi *msi = data;
+
+    return msi->address >> 32;
+}
+
+static void vpci_msi_address_upper_write(const struct pci_dev *pdev,
+                                         unsigned int reg, uint32_t val,
+                                         void *data)
+{
+    struct vpci_msi *msi = data;
+
+    /* Clear high part. */
+    msi->address &= 0xffffffff;
+    msi->address |= (uint64_t)val << 32;
+}
+
+/* Handlers for the data field. */
+static uint32_t vpci_msi_data_read(const struct pci_dev *pdev,
+                                   unsigned int reg, void *data)
+{
+    const struct vpci_msi *msi = data;
+
+    return msi->data;
+}
+
+static void vpci_msi_data_write(const struct pci_dev *pdev, unsigned int reg,
+                                uint32_t val, void *data)
+{
+    struct vpci_msi *msi = data;
+
+    msi->data = val;
+}
+
+/* Handlers for the MSI mask bits. */
+static uint32_t vpci_msi_mask_read(const struct pci_dev *pdev,
+                                   unsigned int reg, void *data)
+{
+    const struct vpci_msi *msi = data;
+
+    return msi->mask;
+}
+
+static void vpci_msi_mask_write(const struct pci_dev *pdev, unsigned int reg,
+                                uint32_t val, void *data)
+{
+    struct vpci_msi *msi = data;
+    uint32_t dmask;
+
+    dmask = msi->mask ^ val;
+
+    if ( !dmask )
+        return;
+
+    if ( msi->enabled )
+    {
+        unsigned int i;
+
+        for ( i = ffs(dmask) - 1; dmask && i < msi->vectors;
+              i = ffs(dmask) - 1 )
+        {
+            vpci_msi_arch_mask(msi, pdev, i, (val >> i) & 1);
+            __clear_bit(i, &dmask);
+        }
+    }
+
+    msi->mask = val;
+}
+
+static int vpci_init_msi(struct pci_dev *pdev)
+{
+    uint8_t seg = pdev->seg, bus = pdev->bus;
+    uint8_t slot = PCI_SLOT(pdev->devfn), func = PCI_FUNC(pdev->devfn);
+    struct vpci_msi *msi;
+    unsigned int pos;
+    uint16_t control;
+    int ret;
+
+    pos = pci_find_cap_offset(seg, bus, slot, func, PCI_CAP_ID_MSI);
+    if ( !pos )
+        return 0;
+
+    msi = xzalloc(struct vpci_msi);
+    if ( !msi )
+        return -ENOMEM;
+
+    msi->pos = pos;
+
+    ret = vpci_add_register(pdev, vpci_msi_control_read,
+                            vpci_msi_control_write,
+                            msi_control_reg(pos), 2, msi);
+    if ( ret )
+    {
+        xfree(msi);
+        return ret;
+    }
+
+    /* Get the maximum number of vectors the device supports. */
+    control = pci_conf_read16(seg, bus, slot, func, msi_control_reg(pos));
+    msi->max_vectors = multi_msi_capable(control);
+    ASSERT(msi->max_vectors <= 32);
+
+    /* The multiple message enable is 0 after reset (1 message enabled). */
+    msi->vectors = 1;
+
+    /* No PIRQ bound yet. */
+    vpci_msi_arch_init(msi);
+
+    msi->address64 = is_64bit_address(control);
+    msi->masking = is_mask_bit_support(control);
+
+    ret = vpci_add_register(pdev, vpci_msi_address_read,
+                            vpci_msi_address_write,
+                            msi_lower_address_reg(pos), 4, msi);
+    if ( ret )
+    {
+        xfree(msi);
+        return ret;
+    }
+
+    ret = vpci_add_register(pdev, vpci_msi_data_read, vpci_msi_data_write,
+                            msi_data_reg(pos, msi->address64), 2,
+                            msi);
+    if ( ret )
+    {
+        xfree(msi);
+        return ret;
+    }
+
+    if ( msi->address64 )
+    {
+        ret = vpci_add_register(pdev, vpci_msi_address_upper_read,
+                                vpci_msi_address_upper_write,
+                                msi_upper_address_reg(pos), 4, msi);
+        if ( ret )
+        {
+            xfree(msi);
+            return ret;
+        }
+    }
+
+    if ( msi->masking )
+    {
+        ret = vpci_add_register(pdev, vpci_msi_mask_read, vpci_msi_mask_write,
+                                msi_mask_bits_reg(pos, msi->address64), 4,
+                                msi);
+        if ( ret )
+        {
+            xfree(msi);
+            return ret;
+        }
+    }
+
+    pdev->vpci->msi = msi;
+
+    return 0;
+}
+REGISTER_VPCI_INIT(vpci_init_msi);
+
+void vpci_dump_msi(void)
+{
+    struct domain *d;
+
+    for_each_domain ( d )
+    {
+        const struct pci_dev *pdev;
+
+        if ( !has_vpci(d) )
+            continue;
+
+        printk("vPCI MSI information for d%d\n", d->domain_id);
+
+        list_for_each_entry ( pdev, &d->arch.pdev_list, domain_list )
+        {
+            uint8_t seg = pdev->seg, bus = pdev->bus;
+            uint8_t slot = PCI_SLOT(pdev->devfn), func = PCI_FUNC(pdev->devfn);
+            const struct vpci_msi *msi = pdev->vpci->msi;
+
+            if ( !spin_trylock(&pdev->vpci->lock) )
+            {
+                printk("Unable to get vPCI lock, skipping\n");
+                continue;
+            }
+
+            if ( msi )
+            {
+                printk("Device %04x:%02x:%02x.%u\n", seg, bus, slot, func);
+
+                printk("  Enabled: %u Supports masking: %u 64-bit addresses: %u\n",
+                       msi->enabled, msi->masking, msi->address64);
+                printk("  Max vectors: %u enabled vectors: %u\n",
+                       msi->max_vectors, msi->vectors);
+
+                vpci_msi_arch_print(msi);
+
+                if ( msi->masking )
+                    printk("  mask=%08x\n", msi->mask);
+            }
+
+            spin_unlock(&pdev->vpci->lock);
+            process_pending_softirqs();
+        }
+    }
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
