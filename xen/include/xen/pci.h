fd6a15ef5279c5ee1fc015fbbf41b077c494d62d
Roger Pau Monne <roger.pau@citrix.com>
2017-10-18 12:19:33 +0100

    pci: split code to size BARs from pci_add_device
    So that it can be called from outside in order to get the size of regular PCI
    BARs. This will be required in order to map the BARs from PCI devices into PVH
    Dom0 p2m.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    Cc: Jan Beulich <jbeulich@suse.com>
    ---
    Changes since v6:
     - Remove the vf and addr local variables.
     - Change the way flags are declared.
     - Move the last bool parameter to the flags field.
    
    Changes since v5:
     - Introduce a flags field for pci_size_mem_bar.
     - Use pci_sbdf_t.
    
    Changes since v4:
     - Restore printing whether the BAR is from a vf.
     - Make the psize pointer parameter not optional.
     - s/u64/uint64_t.
     - Remove some unneeded parentheses.
     - Assert the return value is never 0.
     - Use the newly introduced pci_sbdf_t type.
    
    Changes since v3:
     - Rename function to size BARs to pci_size_mem_bar.
     - Change the parameters passed to the function. Pass the position and
       whether the BAR is the last one, instead of the (base, max_bars,
       *index) tuple.
     - Make the function return the number of BARs consumed (1 for 32b, 2
       for 64b BARs).
     - Change the dprintk back to printk.
     - Do not log another error message in pci_add_device in case
       pci_size_mem_bar fails.

--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -189,6 +189,10 @@ const char *parse_pci(const char *, unsigned int *seg, unsigned int *bus,
 const char *parse_pci_seg(const char *, unsigned int *seg, unsigned int *bus,
                           unsigned int *dev, unsigned int *func, bool *def_seg);
 
+#define PCI_BAR_VF      (1u << 0)
+#define PCI_BAR_LAST    (1u << 1)
+int pci_size_mem_bar(pci_sbdf_t sbdf, unsigned int pos, uint64_t *addr,
+                     uint64_t *size, unsigned int flags);
 
 bool_t pcie_aer_get_firmware_first(const struct pci_dev *);
 
