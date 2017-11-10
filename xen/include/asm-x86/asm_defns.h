8919df115254543a82aa601683b67a008e2bf3dc
Wei Liu <wei.liu2@citrix.com>
2017-11-10 12:36:49 +0000

    x86: introduce ELFNOTE macro
    It is needed later for introducing PVH entry point.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>

--- xen/include/asm-x86/asm_defns.h
+++ xen/include/asm-x86/asm_defns.h
@@ -409,4 +409,16 @@ static always_inline void stac(void)
 #define REX64_PREFIX "rex64/"
 #endif
 
+#define ELFNOTE(name, type, desc)           \
+    .pushsection .note.name               ; \
+    .align 4                              ; \
+    .long 2f - 1f       /* namesz */      ; \
+    .long 4f - 3f       /* descsz */      ; \
+    .long type          /* type   */      ; \
+1:.asciz #name          /* name   */      ; \
+2:.align 4                                ; \
+3:desc                  /* desc   */      ; \
+4:.align 4                                ; \
+    .popsection
+
 #endif /* __X86_ASM_DEFNS_H__ */
