2be31320f6f8e9c5b681357f5402a203e8920f2b
Roger Pau Monne <roger.pau@citrix.com>
2018-10-19 15:08:20 +0100

    x86/hvm: introduce a define for the debug output IO port
    No functional change intended.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Wei Liu <wei.liu2@citrix.com>
    Cc: George Dunlap <George.Dunlap@eu.citrix.com>
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Tim Deegan <tim@xen.org>

--- xen/include/public/arch-x86/xen.h
+++ xen/include/public/arch-x86/xen.h
@@ -346,6 +346,12 @@ struct xen_arch_domainconfig {
 #define XEN_CPUID          XEN_EMULATE_PREFIX "cpuid"
 #endif
 
+/*
+ * Debug console IO port, also called "port E9 hack". Each character written
+ * to this IO port will be printed on the hypervisor console.
+ */
+#define XEN_HVM_DEBUGCONS_IOPORT 0xe9
+
 #endif /* __XEN_PUBLIC_ARCH_X86_XEN_H__ */
 
 /*
