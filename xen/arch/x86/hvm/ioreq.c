9eb962edfd6b93e964825ff07de8cb14c52d8da9
Roger Pau Monne <roger.pau@citrix.com>
2018-10-19 15:08:20 +0100

    vpci: fix execution of long running operations
    BAR map/unmap is a long running operation that needs to be preempted
    in order to avoid overrunning the assigned vCPU time (or even
    triggering the watchdog).
    
    Current logic for this preemption is wrong, and won't work at all for
    AMD since only Intel makes use of hvm_io_pending (and even in that
    case the current code is wrong).
    
    Instead move the code that performs the mapping/unmapping to
    a tasklet and pause the guest vCPU until the {un}mapping is done and
    the memory decoding bit has been toggled.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    [ioreq parts]
    Reviewed-by: Paul Durrant <paul.durrant@citrix.com>
    ---
    Cc: Paul Durrant <paul.durrant@citrix.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Wei Liu <wei.liu2@citrix.com>
    Cc: George Dunlap <George.Dunlap@eu.citrix.com>
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Tim Deegan <tim@xen.org>

--- xen/arch/x86/hvm/ioreq.c
+++ xen/arch/x86/hvm/ioreq.c
@@ -85,9 +85,6 @@ bool hvm_io_pending(struct vcpu *v)
     struct hvm_ioreq_server *s;
     unsigned int id;
 
-    if ( has_vpci(d) && vpci_process_pending(v) )
-        return true;
-
     FOR_EACH_IOREQ_SERVER(d, id, s)
     {
         struct hvm_ioreq_vcpu *sv;
