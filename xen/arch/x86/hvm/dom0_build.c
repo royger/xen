767544d2cd52ceff8fc096f5a123620fa0476ba4
Roger Pau Monne <roger.pau@citrix.com>
2019-02-01 12:13:47 +0100

    keep runstate area mapped

--- xen/arch/x86/hvm/dom0_build.c
+++ xen/arch/x86/hvm/dom0_build.c
@@ -421,7 +421,7 @@ static int __init pvh_setup_p2m(struct domain *d)
     pvh_setup_e820(d, nr_pages);
     do {
         preempted = false;
-        paging_set_allocation(d, dom0_paging_pages(d, nr_pages),
+        paging_set_allocation(d, dom0_paging_pages(d, nr_pages) * 10,
                               &preempted);
         process_pending_softirqs();
     } while ( preempted );
