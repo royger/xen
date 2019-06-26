ebd7d5dda07a92288c5b866418aadd3b3945714a
Roger Pau Monne <roger.pau@citrix.com>
2019-06-26 10:38:44 +0200

    make: simplify setting HOST{CC/CXX}
    Infer the values of HOST{CC/CXX} from CC/CXX if unset, do this in
    StdGNU.mk, together with the rest of the toolchain variables.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

--- Config.mk
+++ Config.mk
@@ -39,22 +39,12 @@ DESTDIR     ?= /
 # Allow phony attribute to be listed as dependency rather than fake target
 .PHONY: .phony
 
-# If we are not cross-compiling, default HOSTC{C/XX} to C{C/XX}
-ifeq ($(XEN_TARGET_ARCH), $(XEN_COMPILE_ARCH))
-HOSTCC ?= $(CC)
-HOSTCXX ?= $(CXX)
-endif
-
 # Use Clang/LLVM instead of GCC?
 clang ?= n
 ifeq ($(clang),n)
 gcc := y
-HOSTCC ?= gcc
-HOSTCXX ?= g++
 else
 gcc := n
-HOSTCC ?= clang
-HOSTCXX ?= clang++
 endif
 
 DEPS_INCLUDE = $(addsuffix .d2, $(basename $(wildcard $(DEPS))))
