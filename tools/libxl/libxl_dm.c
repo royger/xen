royger/xen

commit aacc143006429de46932aabae17c13846c71fa45

tools/libxl: Plumb domain_create_state down into libxl__build_pre()
To fix CPUID handling, libxl__build_pre() is going to have to distinguish
between a brand new VM vs one which is being migrated-in/resumed.

No functional change.

Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

tools/libxl/libxl_dm.c
@@ -2097,8 +2097,9 @@ char *libxl__stub_dm_name(libxl__gc *gc, const char *guest_name)
     return GCSPRINTF("%s-dm", guest_name);
 }
 
-void libxl__spawn_stub_dm(libxl__egc *egc, libxl__stub_dm_spawn_state *sdss)
+void libxl__spawn_stub_dm(libxl__egc *egc, libxl__domain_create_state *dcs)
 {
+    libxl__stub_dm_spawn_state *sdss = &dcs->sdss;
     STATE_AO_GC(sdss->dm.spawn.ao);
     libxl_ctx *ctx = libxl__gc_owner(gc);
     int ret;
@@ -2197,7 +2198,7 @@ void libxl__spawn_stub_dm(libxl__egc *egc, libxl__stub_dm_spawn_state *sdss)
     if (ret)
         goto out;
     uint32_t dm_domid = sdss->pvqemu.guest_domid;
-    ret = libxl__domain_build(gc, dm_config, dm_domid, stubdom_state);
+    ret = libxl__domain_build(gc, dm_domid, dcs);
     if (ret)
         goto out;
 

tools/libxl/libxl_internal.h
@@ -1389,8 +1389,7 @@ _hidden void libxl__domain_build_state_init(libxl__domain_build_state *s);
 _hidden void libxl__domain_build_state_dispose(libxl__domain_build_state *s);
 
 _hidden int libxl__build_pre(libxl__gc *gc, uint32_t domid,
-              libxl_domain_config * const d_config,
-              libxl__domain_build_state *state);
+                             libxl__domain_create_state *dcs);
 _hidden int libxl__build_post(libxl__gc *gc, uint32_t domid,
                libxl_domain_build_info *info, libxl__domain_build_state *state,
                char **vms_ents, char **local_ents);
@@ -1960,10 +1959,8 @@ _hidden int libxl__domain_make(libxl__gc *gc,
                                libxl__domain_build_state *state,
                                uint32_t *domid);
 
-_hidden int libxl__domain_build(libxl__gc *gc,
-                                libxl_domain_config *d_config,
-                                uint32_t domid,
-                                libxl__domain_build_state *state);
+_hidden int libxl__domain_build(libxl__gc *gc, uint32_t domid,
+                                libxl__domain_create_state *dcs);
 
 /* for device model creation */
 _hidden const char *libxl__domain_device_model(libxl__gc *gc,
@@ -4113,7 +4110,7 @@ typedef struct {
     libxl__xswait_state xswait;
 } libxl__stub_dm_spawn_state;
 
-_hidden void libxl__spawn_stub_dm(libxl__egc *egc, libxl__stub_dm_spawn_state*);
+_hidden void libxl__spawn_stub_dm(libxl__egc *egc, libxl__domain_create_state *dcs);
 
 _hidden char *libxl__stub_dm_name(libxl__gc *gc, const char * guest_name);
 

tools/libxl/libxl_create.c
@@ -431,17 +431,17 @@ static void init_console_info(libxl__gc *gc,
        Only 'channels' when mapped to consoles have a string name. */
 }
 
-int libxl__domain_build(libxl__gc *gc,
-                        libxl_domain_config *d_config,
-                        uint32_t domid,
-                        libxl__domain_build_state *state)
+int libxl__domain_build(libxl__gc *gc, uint32_t domid,
+                        libxl__domain_create_state *dcs)
 {
+    libxl_domain_config *d_config = dcs->guest_config;
     libxl_domain_build_info *const info = &d_config->b_info;
+    libxl__domain_build_state *state = &dcs->build_state;
     char **vments = NULL, **localents = NULL;
     struct timeval start_time;
     int i, ret;
 
-    ret = libxl__build_pre(gc, domid, d_config, state);
+    ret = libxl__build_pre(gc, domid, dcs);
     if (ret)
         goto out;
 
@@ -1218,7 +1218,7 @@ static void domcreate_bootloader_done(libxl__egc *egc,
     dcs->sdss.callback = domcreate_devmodel_started;
 
     if (restore_fd < 0 && dcs->domid_soft_reset == INVALID_DOMID) {
-        rc = libxl__domain_build(gc, d_config, domid, state);
+        rc = libxl__domain_build(gc, domid, dcs);
         domcreate_rebuild_done(egc, dcs, rc);
         return;
     }
@@ -1244,7 +1244,7 @@ static void domcreate_bootloader_done(libxl__egc *egc,
         goto out;
     }
 
-    rc = libxl__build_pre(gc, domid, d_config, state);
+    rc = libxl__build_pre(gc, domid, dcs);
     if (rc)
         goto out;
 
@@ -1554,7 +1554,7 @@ static void domcreate_launch_dm(libxl__egc *egc, libxl__multidev *multidev,
 
         dcs->sdss.dm.guest_domid = domid;
         if (libxl_defbool_val(d_config->b_info.device_model_stubdomain))
-            libxl__spawn_stub_dm(egc, &dcs->sdss);
+            libxl__spawn_stub_dm(egc, dcs);
         else
             libxl__spawn_local_dm(egc, &dcs->sdss.dm);
 

tools/libxl/libxl_dom.c
@@ -387,9 +387,11 @@ static void hvm_set_conf_params(xc_interface *handle, uint32_t domid,
 }
 
 int libxl__build_pre(libxl__gc *gc, uint32_t domid,
-              libxl_domain_config *d_config, libxl__domain_build_state *state)
+                     libxl__domain_create_state *dcs)
 {
+    libxl_domain_config *d_config = dcs->guest_config;
     libxl_domain_build_info *const info = &d_config->b_info;
+    libxl__domain_build_state *state = &dcs->build_state;
     libxl_ctx *ctx = libxl__gc_owner(gc);
     char *xs_domid, *con_domid;
     int rc;

commit c5072e46f1dfa5b0877312d0fddec358123dee14

libxl: fix stubdomain creation after aacc143006429de
aacc143006429de broke stubdomain creation by passing the guest
domain_create_state to libxl__domain_build in libxl__spawn_stub_dm,
when it should instead be crafting a new domain_create_state for the
studdomain.

Fixes: aacc143006429de ('tools/libxl: Plumb domain_create_state down into libxl__build_pre()')
Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

tools/libxl/libxl_dm.c
@@ -2110,17 +2110,21 @@ void libxl__spawn_stub_dm(libxl__egc *egc, libxl__domain_create_state *dcs)
     xs_transaction_t t;
 
     /* convenience aliases */
-    libxl_domain_config *const dm_config = &sdss->dm_config;
     libxl_domain_config *const guest_config = sdss->dm.guest_config;
     const int guest_domid = sdss->dm.guest_domid;
     libxl__domain_build_state *const d_state = sdss->dm.build_state;
-    libxl__domain_build_state *const stubdom_state = &sdss->dm_state;
+    libxl__domain_build_state *stubdom_state;
+    libxl_domain_config *dm_config;
 
     /* Initialise private part of sdss */
-    libxl__domain_build_state_init(stubdom_state);
     dmss_init(&sdss->dm);
     dmss_init(&sdss->pvqemu);
     libxl__xswait_init(&sdss->xswait);
+    GCNEW(sdss->dcs);
+    stubdom_state = &sdss->dcs->build_state;
+    libxl__domain_build_state_init(stubdom_state);
+    GCNEW(sdss->dcs->guest_config);
+    dm_config = sdss->dcs->guest_config;
 
     if (guest_config->b_info.device_model_version !=
         LIBXL_DEVICE_MODEL_VERSION_QEMU_XEN_TRADITIONAL) {
@@ -2198,7 +2202,7 @@ void libxl__spawn_stub_dm(libxl__egc *egc, libxl__domain_create_state *dcs)
     if (ret)
         goto out;
     uint32_t dm_domid = sdss->pvqemu.guest_domid;
-    ret = libxl__domain_build(gc, dm_domid, dcs);
+    ret = libxl__domain_build(gc, dm_domid, sdss->dcs);
     if (ret)
         goto out;
 
@@ -2264,11 +2268,11 @@ static void spawn_stub_launch_dm(libxl__egc *egc,
     libxl__device_console *console;
 
     /* convenience aliases */
-    libxl_domain_config *const dm_config = &sdss->dm_config;
+    libxl_domain_config *const dm_config = sdss->dcs->guest_config;
     libxl_domain_config *const guest_config = sdss->dm.guest_config;
     const int guest_domid = sdss->dm.guest_domid;
     libxl__domain_build_state *const d_state = sdss->dm.build_state;
-    libxl__domain_build_state *const stubdom_state = &sdss->dm_state;
+    libxl__domain_build_state *const stubdom_state = &sdss->dcs->build_state;
     uint32_t dm_domid = sdss->pvqemu.guest_domid;
     int need_qemu;
 
@@ -2354,8 +2358,8 @@ static void spawn_stub_launch_dm(libxl__egc *egc,
 
     sdss->pvqemu.spawn.ao = ao;
     sdss->pvqemu.guest_domid = dm_domid;
-    sdss->pvqemu.guest_config = &sdss->dm_config;
-    sdss->pvqemu.build_state = &sdss->dm_state;
+    sdss->pvqemu.guest_config = sdss->dcs->guest_config;
+    sdss->pvqemu.build_state = &sdss->dcs->build_state;
     sdss->pvqemu.callback = spawn_stubdom_pvqemu_cb;
 
     if (!need_qemu) {
@@ -2464,7 +2468,7 @@ static void stubdom_xswait_cb(libxl__egc *egc, libxl__xswait_state *xswait,
     if (strcmp(p, "running"))
         return;
  out:
-    libxl__domain_build_state_dispose(&sdss->dm_state);
+    libxl__domain_build_state_dispose(&sdss->dcs->build_state);
     libxl__xswait_stop(gc, xswait);
     dmss_dispose(gc, &sdss->dm);
     dmss_dispose(gc, &sdss->pvqemu);

tools/libxl/libxl_internal.h
@@ -4102,8 +4102,7 @@ typedef struct {
     /* filled in by user, must remain valid: */
     libxl__dm_spawn_cb *callback; /* called as callback(,&sdss->dm,) */
     /* private to libxl__spawn_stub_dm: */
-    libxl_domain_config dm_config;
-    libxl__domain_build_state dm_state;
+    libxl__domain_create_state *dcs;
     libxl__dm_spawn_state pvqemu;
     libxl__destroy_domid_state dis;
     libxl__multidev multidev;