bf42f52ac6dca8702f84dbc023d2f5ee6c2536d2
Roger Pau Monne <roger.pau@citrix.com>
2024-10-21 11:44:04 +0200

    x86/pv: introduce function to populate perdomain area and use it to map Xen GDT
    The current code to update the Xen part of the GDT when running a PV guest
    relies on caching the direct map address of all the L1 tables used to map the
    GDT and LDT, so that entries can be modified.
    
    Introduce a new function that populates the per-domain region, either using the
    recursive linear mappings when the target vCPU is the current one, or by
    directly modifying the L1 table of the per-domain region.
    
    Using such function to populate per-domain addresses drops the need to keep a
    reference to per-domain L1 tables previously used to change the per-domain
    mappings.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/arch/x86/include/asm/mm.h
+++ xen/arch/x86/include/asm/mm.h
@@ -603,6 +603,8 @@ int compat_arch_memory_op(unsigned long cmd, XEN_GUEST_HANDLE_PARAM(void) arg);
 int create_perdomain_mapping(struct domain *d, unsigned long va,
                              unsigned int nr, l1_pgentry_t **pl1tab,
                              struct page_info **ppg);
+void populate_perdomain_mapping(const struct vcpu *v, unsigned long va,
+                                mfn_t *mfn, unsigned long nr);
 void destroy_perdomain_mapping(struct domain *d, unsigned long va,
                                unsigned int nr);
 void free_perdomain_mappings(struct domain *d);
