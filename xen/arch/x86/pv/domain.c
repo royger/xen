c69044ce6489cb2a4b5f4ceb378a6252011077db
Roger Pau Monne <roger.pau@citrix.com>
2024-10-22 13:47:55 +0200

    x86/pv: update guest LDT mappings using the linear entries
    The pv_map_ldt_shadow_page() and pv_destroy_ldt() functions rely on the L1
    table(s) that contain such mappings being stashed in the domain structure, and
    thus such mappings being modified by merely updating the require L1 entries.
    
    Switch pv_map_ldt_shadow_page() to unconditionally use the linear recursive, as
    that logic is always called while the vCPU is running on the current pCPU.
    
    For pv_destroy_ldt() use the linear mappings if the vCPU is the one currently
    running on the pCPU, otherwise use destroy_mappings().
    
    Note this requires keeping an array with the pages currently mapped at the LDT
    area, as that allows dropping the extra taken page reference when removing the
    mappings.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/arch/x86/pv/domain.c
+++ xen/arch/x86/pv/domain.c
@@ -353,6 +353,7 @@ void pv_vcpu_destroy(struct vcpu *v)
 int pv_vcpu_initialise(struct vcpu *v)
 {
     struct domain *d = v->domain;
+    unsigned int i;
     int rc;
 
     ASSERT(!is_idle_domain(d));
@@ -360,6 +361,9 @@ int pv_vcpu_initialise(struct vcpu *v)
     rc = pv_create_gdt_ldt_l1tab(v);
     if ( rc )
         return rc;
+
+    for ( i = 0; i < ARRAY_SIZE(v->arch.pv.ldt_frames); i++ )
+        v->arch.pv.ldt_frames[i] = INVALID_MFN;
 
     if ( v->domain->arch.pv.xpti )
     {
