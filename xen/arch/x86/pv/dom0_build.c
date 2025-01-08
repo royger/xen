7582b3e2be95fefbeaf8848629167034896549d0
Wei Liu <wei.liu2@citrix.com>
2025-01-08 15:18:09 +0000

    x86/pv: Use copy_domain_page() to manage domheap pages during initrd
    relocation
    
    Replace the manual copying logic with a call to `copy_domain_page()`
    while relocating intird which map and unmap pages accordingly.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Signed-off-by: Wei Wang <wawei@amazon.de>
    Signed-off-by: Julien Grall <jgrall@amazon.com>
    Signed-off-by: Elias El Yandouzi <eliasely@amazon.com>
    Signed-off-by: Alejandro Vallejo <alejandro.vallejo@cloud.com>

--- xen/arch/x86/pv/dom0_build.c
+++ xen/arch/x86/pv/dom0_build.c
@@ -631,17 +631,24 @@ static int __init dom0_construct(const struct boot_domain *bd)
         if ( d->arch.pv.physaddr_bitsize &&
              ((mfn + count - 1) >> (d->arch.pv.physaddr_bitsize - PAGE_SHIFT)) )
         {
+            unsigned int nr_pages = count;
+
             order = get_order_from_pages(count);
             page = alloc_domheap_pages(d, order, MEMF_no_scrub);
             if ( !page )
                 panic("Not enough RAM for domain 0 initrd\n");
+
             for ( count = -count; order--; )
                 if ( count & (1UL << order) )
                 {
                     free_domheap_pages(page, order);
                     page += 1UL << order;
+                    nr_pages -= 1UL << order;
                 }
-            memcpy(page_to_virt(page), __va(initrd->start), initrd_len);
+
+            for ( ; nr_pages-- ; page++, mfn++ )
+                copy_domain_page(page_to_mfn(page), _mfn(mfn));
+
             /*
              * The initrd was copied but the initrd variable is reused in the
              * calculations below. As to not leak the memory used for the
