4ea913f13f347e20be96e53b05dcd8243b5d74bc
Roger Pau Monne <roger.pau@citrix.com>
2025-06-09 16:43:44 +0200

    pdx: provide a unified set of unit functions
    The current setup (pdx_init_mask() and pdx_region_mask()) and init
    (pfn_pdx_hole_setup()) PDX compression functions are tailored to the
    existing PDX compression algorithm.
    
    In preparation for introducing a new compression algorithm convert the
    setup and init functions to more generic interfaces that aren't tied to the
    compression in-use.  To accomplish this introduce a function that registers
    all the PFN RAM ranges, plus an init function.
    
    This has the downside of requiring a static array to store such ranges
    ahead of being processed by the setup function, however it's the only way
    to pass all the possible information to the different compression setup
    functions without using per-compression specific setup functions.
    Per-arch compression setup also need to be adjusted to use the new
    interface.  There's a slight ordering adjustment, in that after PDX
    compression setup the caller will check whether all the RAM regions are
    properly covered by the newly setup compression, otherwise compression is
    disabled by resetting to the initial values.
    
    No functional change intended in the resulting PDX compression values.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    ---
    Changes since v2:
     - Fix indentation.
    
    Changes since v1:
     - s/nr/nr_ranges/

--- xen/arch/arm/setup.c
+++ xen/arch/arm/setup.c
@@ -256,9 +256,11 @@ void __init init_pdx(void)
 {
     const struct membanks *mem = bootinfo_get_mem();
     paddr_t bank_start, bank_size, bank_end, ram_end = 0;
-    int bank;
+    unsigned int bank;
 
 #ifndef CONFIG_PDX_NONE
+    for ( bank = 0 ; bank < mem->nr_banks; bank++ )
+        pfn_pdx_add_region(mem->bank[bank].start, mem->bank[bank].size);
     /*
      * Arm does not have any restrictions on the bits to compress. Pass 0 to
      * let the common code further restrict the mask.
@@ -266,26 +268,24 @@ void __init init_pdx(void)
      * If the logic changes in pfn_pdx_hole_setup we might have to
      * update this function too.
      */
-    uint64_t mask = pdx_init_mask(0x0);
+    pfn_pdx_compression_setup(0);
 
     for ( bank = 0 ; bank < mem->nr_banks; bank++ )
     {
-        bank_start = mem->bank[bank].start;
-        bank_size = mem->bank[bank].size;
-
-        mask |= bank_start | pdx_region_mask(bank_start, bank_size);
+        if ( !pdx_is_region_compressible(
+                  mem->bank[bank].start,
+                  PFN_UP(mem->bank[bank].start + mem->bank[bank].size) -
+                  PFN_DOWN(mem->bank[bank].start)) )
+        {
+            pfn_pdx_compression_reset();
+            printk(XENLOG_WARNING
+                   "PFN compression disabled, RAM region [%#" PRIpaddr ", %#"
+                   PRIpaddr "] not covered\n",
+                   mem->bank[bank].start,
+                   mem->bank[bank].start + mem->bank[bank].size - 1);
+            break;
+        }
     }
-
-    for ( bank = 0 ; bank < mem->nr_banks; bank++ )
-    {
-        bank_start = mem->bank[bank].start;
-        bank_size = mem->bank[bank].size;
-
-        if (~mask & pdx_region_mask(bank_start, bank_size))
-            mask = 0;
-    }
-
-    pfn_pdx_hole_setup(mask >> PAGE_SHIFT);
 #endif
 
     for ( bank = 0 ; bank < mem->nr_banks; bank++ )
