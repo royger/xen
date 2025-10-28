cb9d3cbf3976f73f9de27513532e9902b11dc9f2
Frediano Ziglio <frediano.ziglio@citrix.com>
2025-10-28 14:53:31 +0100

    xen/mm: allow alloc_heap_pages() calls with current == NULL
    Upcoming changes will introduce boot time calls to allocate memory from
    domheap ahead of having created the idle domain, and then current is still
    NULL.  Modify alloc_heap_pages() to cope with current == NULL.
    
    Signed-off-by: Frediano Ziglio <frediano.ziglio@citrix.com>
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/common/page_alloc.c
+++ xen/common/page_alloc.c
@@ -1108,7 +1108,7 @@ static struct page_info *alloc_heap_pages(
     if ( first_dirty != INVALID_DIRTY_IDX ||
          (scrub_debug && !(memflags & MEMF_no_scrub)) )
     {
-        bool cold = d && d != current->domain;
+        bool cold = d && current && d != current->domain;
 
         for ( i = 0; i < (1U << order); i++ )
         {
@@ -2086,7 +2086,7 @@ static struct page_info *alloc_color_heap_page(unsigned int memflags,
     if ( !(memflags & MEMF_no_scrub) )
     {
         if ( need_scrub )
-            scrub_one_page(pg, d != current->domain);
+            scrub_one_page(pg, current && d != current->domain);
         else
             check_one_page(pg);
     }
